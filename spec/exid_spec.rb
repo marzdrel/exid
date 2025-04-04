5c21a1842bf14c3aa796b2b2512c04e02dd568bd
Marz Drel <marzdrel@users.noreply.github.com>
2025-04-04 15:12:14 +0200

    Initial commit

---
+++ spec/exid_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Exid do
+  it "has a version number" do
+    expect(Exid::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
